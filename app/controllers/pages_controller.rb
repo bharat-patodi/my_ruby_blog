46e271e30ebeed4183e065b5af6339ad24af34ce
bharatpatodi <bharatpatodi@gmail.com>
2019-12-29 18:54:16 +0530

    First commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+    # Home action
+    def home; end
+
+    # About action
+    def about
+    end
+
+    # Contact action
+    def contact
+    end
+
+end
